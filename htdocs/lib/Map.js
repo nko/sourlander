477d65a9ca66ec26efb8a7c6aedfdb47285dd99e
Mark Schmale <ma.schmale@googlemail.com>
2010-08-28 15:23:31 +0200

    map creation is nice now.

--- htdocs/lib/Map.js
+++ htdocs/lib/Map.js
@@ -1,17 +1,14 @@
-var Map = function Map(sel) {
+var Map = function Map(sel, seed) {
     this.size  = {x: 720, y: 400};
     this.paper = Raphael(sel, this.size.x, this.size.y);
     this.paper.rect(0, 0, this.size.x, this.size.y);
-    
     this.points = [];
-    var r0 = Math.random() * 180;
-    var r1 = Math.random() * 1 - 0.5;
     for(i=0;i<this.size.x;i++) {
         this.points[i] = {
                           x: i
-                        , y:  (this.size.y - Math.sin((i+90) * Math.PI / 180)*50)-100
-                            + Math.sin((i+r0)*10 * Math.PI / 180)*2
-                            + Math.sin((i+180-r0)*r1 * Math.PI / 180)*30
+                        , y:  (this.size.y - Math.sin((i+90) * Math.PI / 180)*(-seed[2]))-100
+                                + Math.sin((i+seed[0])*10 * Math.PI / 180)*2
+                                + Math.sin((i+seed[0]+45)*seed[1] * Math.PI / 180)*1
                         };
     }
     this.draw();
