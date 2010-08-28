e6aac8d9f99484b567d6e681546674749bffb4d8
Mark Schmale <ma.schmale@googlemail.com>
2010-08-28 04:11:06 +0200

    moved Client logic to lib/GameClient

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var http   = require('http'),
-    ws     = require('./lib/webserver'),
-    io     = require('./lib/socket.io'),
-    sys    = require('sys'),
-    config = require('./config');
+var http    = require('http'),
+    ws      = require('./lib/webserver'),
+    io      = require('./lib/socket.io'),
+    GClient = require('./lib/GameClient'),
+    sys     = require('sys'),
+    config  = require('./config');
 
 var web = http.createServer(ws)
 var socket = io.listen(web);
@@ -10,10 +11,5 @@ var socket = io.listen(web);
 web.listen(config.webport);
 
 socket.on('connection', function(client) {
-    sys.debug('client connected');
-    client.send('welcome!');
-
-    client.on('message', function(data) {
-        sys.debug('MSG: ' + data);
-    });
+    var gc = new GClient(client);
 });
