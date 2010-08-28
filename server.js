13b667a211b752ce6b85c83991ab9593f4306760
Mark Schmale <ma.schmale@googlemail.com>
2010-08-28 04:06:18 +0200

    cleaning up & adding some debug to WebSocket Server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http   = require('http'),
-    ws     = require('./webserver'),
+    ws     = require('./lib/webserver'),
     io     = require('./lib/socket.io'),
+    sys    = require('sys'),
     config = require('./config');
 
 var web = http.createServer(ws)
@@ -9,5 +10,10 @@ var socket = io.listen(web);
 web.listen(config.webport);
 
 socket.on('connection', function(client) {
-    
+    sys.debug('client connected');
+    client.send('welcome!');
+
+    client.on('message', function(data) {
+        sys.debug('MSG: ' + data);
+    });
 });
