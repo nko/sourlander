9d12ab50b1f05f2fa4022edf9db9acd144b95562
Mark Schmale <ma.schmale@googlemail.com>
2010-08-28 03:48:39 +0200

    moved socket.io clientlib vom htdocs/socket.io  to htdocs/lib

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var http   = require('http'),
     ws     = require('./webserver'),
+    io     = require('./lib/socket.io'),
     config = require('./config');
 
 var web = http.createServer(ws).listen(config.webport);
+var socket = io.listen(web);
+
+socket.on('connection', function(client) {
+    
+});
